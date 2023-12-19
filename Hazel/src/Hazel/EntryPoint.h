9fdff61086e6d253ef02f7208438657484e8e172
Sato <satou522satou@gmail.com>
2023-12-19 22:19:44 +0900

    made Entry point

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,9 +6,14 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) 
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_CLIENT_INFO("Hello! Var={0}", 5);
+
 	auto app = Hazel::CreateApplication();
 	app ->Run();
 	delete app;
 }
+
 
 #endif
